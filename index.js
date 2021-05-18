92b1fffaee728d2e8160b67419c0a95a2b97e0aa
antoniopresto <antoniopresto@gmail.com>
2021-05-18 14:15:22 -0300

    fix: return null values

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function _(o, qs) {
   var qa = qs.replace(/\[([0-9]*)]/gim, '.$1').split('.');
   return qa.reduce(function(prev, next) {
-    if (!prev) return undefined;
+    if (!prev) return prev;
     return prev[next];
   }, o);
 };
